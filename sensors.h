79ae1d6652d55dec6065458ab2d41d654ed5f627
Alex Redmond <42391315+AlexFF000@users.noreply.github.com>
2021-04-07 16:45:58 +0100

    Added sensors.h file

---
+++ sensors.h
@@ -0,0 +1,11 @@
+/*
+    Alex Redmond, Group R
+
+    Header file for declarations related to reading the sensors
+ */
+
+ struct readings{
+     float temperature;
+     float pressure;
+     float lightLevel;
+ };
